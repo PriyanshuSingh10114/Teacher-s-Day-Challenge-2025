25f02ea683c5e511ca0a290e3da6bf0d6978a359
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-07-29 12:25:28 +0530

    Update july_28.cpp

--- july_28.cpp
+++ july_28.cpp
@@ -35,3 +35,42 @@ public:
         return max_profit;
     }
 };
+
+
+
+// 155. Min Stack
+class MinStack {
+public:
+    stack<int>st;
+    stack<int>min_st;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        
+        if(min_st.empty()){
+            min_st.push(val);
+        }
+        else if(min_st.top()>=val){
+            min_st.push(val);
+        }
+    }
+    
+    void pop() {
+        if(st.top()==min_st.top()){
+            min_st.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min_st.top();
+    }
+};

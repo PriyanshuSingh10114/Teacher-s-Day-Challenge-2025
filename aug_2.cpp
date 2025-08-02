8de21fd15a5fefcb8ca66def1dcf84d8158865f3
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-03 01:34:56 +0530

    Update aug_2.cpp

--- aug_2.cpp
+++ aug_2.cpp
@@ -1,1 +1,46 @@
+//Beginner's Track
 
+//A. Team
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    while(n){
+        int p,v,t;
+        cin>>p>>v>>t;
+        if(p==1 && v==1 && t==1 || p==1 && v==1 && t==0 ||p==0 && v==1 && t==1 ||p==1 && v==0 && t==1 ){
+        count++;
+        }
+        n--;
+    }
+    cout<<count<<endl;
+ 
+return 0;
+}
+
+
+//Advance Track
+
+//A. Team
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    while(n){
+        int p,v,t;
+        cin>>p>>v>>t;
+        if(p==1 && v==1 && t==1 || p==1 && v==1 && t==0 ||p==0 && v==1 && t==1 ||p==1 && v==0 && t==1 ){
+        count++;
+        }
+        n--;
+    }
+    cout<<count<<endl;
+ 
+return 0;
+}

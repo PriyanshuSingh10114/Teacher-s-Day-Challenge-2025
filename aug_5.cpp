92fe9a04a99524321083108f1e82bf873c0f3f4c
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-05 14:23:20 +0530

    Update aug_5.cpp

--- aug_5.cpp
+++ aug_5.cpp
@@ -27,3 +27,27 @@ return 0;
 
 //Advance Track
 
+//Boy or Girl
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+
+    set<char>uniqueStr;
+    for(char ch:str){
+        uniqueStr.insert(ch);
+    }
+
+    int uniqueLength=uniqueStr.size();
+
+    if(uniqueLength%2==0){
+        cout<<"CHAT WITH HER!"<<endl;
+    }
+    else{
+        cout<<"IGNORE HIM!"<<endl;
+    }
+    
+return 0;
+}

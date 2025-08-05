8f09da9d8149804e74a5e8d48b51861844f2e88e
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-05 12:35:30 +0530

    Update aug_4.cpp

--- aug_4.cpp
+++ aug_4.cpp
@@ -1,1 +1,50 @@
+//Advanced Track
 
+//A. George and Accommodation
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=0;i<n;i++){
+        int p,q;
+        cin>>p>>q;
+        if(p<q && q-p >=2){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+ 
+return 0;
+}
+
+//A. Word 59A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int uc=0;
+    int lc=0;
+    for(char ch:s){
+        if(isupper(ch)){
+            uc++;
+        }
+        else if(islower(ch)){
+            lc++;
+        }
+    }
+    if(uc>lc){
+       transform(s.begin(),s.end(),s.begin(),::toupper);
+       cout<<s<<endl;
+    }
+    else{
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        cout<<s<<endl;
+    }
+
+return 0;
+}

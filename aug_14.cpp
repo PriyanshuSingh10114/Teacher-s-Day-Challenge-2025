c92cd69d7c7e5046fa9be7f3f65205b4804a8392
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-15 22:40:46 +0530

    Update aug_14.cpp

--- aug_14.cpp
+++ aug_14.cpp
@@ -2,3 +2,32 @@
 
 //Twins
 
+
+
+//Advanced Track - 43 Days Journey
+
+//Bit++  282A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int X=0;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        if(s=="X++" ||s=="++X"){
+            X++;
+        }
+        else if(s=="X--" ||s=="--X"){
+            X--;
+        }
+    }
+    cout<<X<<endl;
+ 
+return 0;
+}
+
+// A. Even Odds 318 A
+

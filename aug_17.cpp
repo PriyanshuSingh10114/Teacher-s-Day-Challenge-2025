1c99afa9e69ea027b03ac8306314714a3215812e
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-23 01:01:43 +0530

    Update aug_17.cpp

--- aug_17.cpp
+++ aug_17.cpp
@@ -1,1 +1,16 @@
+// Beginner's Track - 43 Days Journey
 
+//A. Soldier and Bananas
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k,n,w;
+    cin>>k>>n>>w;
+
+    long long total=k*w*(w+1)/2;
+    long long borrow=max(0LL,total-n);
+
+    cout<<borrow<<endl;
+return 0;
+}

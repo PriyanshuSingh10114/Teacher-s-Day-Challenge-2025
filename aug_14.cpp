9d9f09ad37f519ff322252a561170a92742130a5
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-23 00:08:37 +0530

    Update aug_14.cpp

--- aug_14.cpp
+++ aug_14.cpp
@@ -2,6 +2,31 @@
 
 //Twins
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>coins(n);
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+    }
+    
+    sort(coins.begin(),coins.end(),greater<int>());
+
+    int total=accumulate(coins.begin(),coins.end(),0);
+    int ans=0;
+    int count=0;
+    for(int i=0;i<n;i++){
+        ans=ans+coins[i];
+        count++;
+        if(ans>total/2){
+            break;
+        }
+    }
+    cout<<count<<endl;
+return 0;
+}
 
 
 //Advanced Track - 43 Days Journey

869bc92a3dfe829cbf3ecfdc9f7540f835d1d5dd
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-23 00:39:39 +0530

    Update aug_16.cpp

--- aug_16.cpp
+++ aug_16.cpp
@@ -1,1 +1,19 @@
+//Beginner's Track - 43 Days Journey
 
+//Elephant
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int moves=n/5;
+    if(n%5!=0){
+        moves++;
+    }
+
+    cout<<moves<<endl;
+
+return 0;
+}

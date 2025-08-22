980271ad0c53344fc0913f9f3788fc5167663949
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-22 23:48:20 +0530

    Update aug_18.cpp

--- aug_18.cpp
+++ aug_18.cpp
@@ -1,1 +1,29 @@
+//Beginner's Track - 43 Days Journey
 
+//Word
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int uc=0;
+    int lc=0;
+    for(char ch:s){
+        if(isupper(ch)){
+            uc++;
+        }
+        else if(islower(ch)){
+            lc++;
+        }
+    }
+    if(uc>lc){
+       transform(s.begin(),s.end(),s.begin(),::toupper);
+       cout<<s<<endl;
+    }
+    else{
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        cout<<s<<endl;
+    }
+ 
+re

a541fc76de2480cfae14d4cc90c64a1d3ba3f863
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-08-26 21:32:43 +0530

    Update aug_23.cpp

--- aug_23.cpp
+++ aug_23.cpp
@@ -1,1 +1,21 @@
 
+//Beginner's Track - 43 Days Journey
+
+//Bear and Big Brother
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int years=0;
+
+    while(a<=b){
+       a*=3;
+       b*=2;
+       years++; 
+    }
+    cout<<years<<endl;
+    
+return 0;
+}

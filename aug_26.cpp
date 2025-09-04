b16b221ae015610aa105e160edc4585541301324
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-09-04 09:27:44 +0530

    Update aug_26.cpp

--- aug_26.cpp
+++ aug_26.cpp
@@ -1,1 +1,24 @@
 
+//Advanced Track - 43 Days Journey
+
+//Divisbility Problem
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        int a,b;
+        cin>>a>>b;
+        if(a%b==0){
+            cout<<"0"<<endl;
+        }
+        else{
+            int n=b-a%b;
+            cout<<n<<endl;
+        }
+    }
+
+return 0;
+}

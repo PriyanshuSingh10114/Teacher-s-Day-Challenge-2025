152577187080625bf2e4d8aae540aca5db820f0f
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-09-06 01:49:18 +0530

    Update sep_4.cpp

--- sep_4.cpp
+++ sep_4.cpp
@@ -1,1 +1,15 @@
+//Beginner's Track - 43 Days Journey
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n/2<<endl;
+    }
+    else{
+        cout<<-(n+1)/2<<endl;
+    }
+return 0;
+}

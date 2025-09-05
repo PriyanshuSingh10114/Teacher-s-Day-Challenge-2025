a4cf3887b553e2e767faf9834d45b7cae445cb75
Priyanshu Singh <priyanshusingh22340@gmail.com>
2025-09-05 11:50:04 +0530

    Update sep_5.cpp

--- sep_5.cpp
+++ sep_5.cpp
@@ -1,4 +1,56 @@
 
+//Beginner's Track - 43 Days Journey 
+
+//caps Lock
+
+#include <bits/stdc++.h>
+using namespace std;
+bool checkAllUpper(string& s){
+    int n=s.size();
+    for(int i=0;i<n;i++){
+        if(islower(s[i])){
+            return false;
+        }
+    }
+    return true;
+}
+bool checkFirst(string& s){
+    int n=s.size();
+    if(islower(s[0])){
+        for(int i=1;i<n;i++){
+            if(islower(s[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+string Swap(string& s){
+    string res="";
+    int n=s.size();
+    for(int i=0;i<n;i++){
+        if(islower(s[i])){
+            res+=(char)toupper(s[i]);
+        }
+        else{
+            res+=(char)tolower(s[i]);
+        }
+    }
+    return res;
+}
+int main(){
+    string s;
+    cin>>s;
+    if(checkAllUpper(s) || checkFirst(s)){
+        cout<<Swap(s);
+    }
+    else{
+        cout<<s;
+    }
+}
+
+
 //Advanced Track - 43 Days Journey
 
 //Magic Numbers
